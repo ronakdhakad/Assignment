04435df73d7191d74b9b0ad2cf669d5b0a88a105
ronakdhakad <dhakadr932@gmail.com>
2025-09-01 17:31:27 +0530

    f

---
+++ assignment5/Q119.java
@@ -0,0 +1,13 @@
+class Main{
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=6-i;j++){
+                System.out.print(" ");
+            }
+            for(char k=65;k<=64+i;k++){
+                System.out.print(k+" ");
+            }
+            System.out.println();
+        }
+    }    
+}
