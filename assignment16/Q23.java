5b7d1bd67b7009b2fb8520eeb758f175862feed1
ronakdhakad <dhakadr932@gmail.com>
2025-09-05 17:01:11 +0530

    remote

--- assignment16/Q23.java
+++ assignment16/Q23.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.Scanner;
 class AccountHolder{
     String name;
@@ -178,5 +179,51 @@ class Main{
         obj.getHolderDetails();
         obj.MenuA();
         obj.MenuB();
+=======
+class Joint{
+    String email;
+    String password;
+    String name;
+    int age;
+    int jAmt=5000;
+
+    void Joint(String email,String password,String name,int age){
+        this.email=email;
+        this.password=password;
+        this.name=name;
+        this.age=age;
+    }
+}
+class Saving{
+    String email;
+    String password;
+    String name;
+    int age;
+    int jAmt=2000;
+    void Saving(String email,String password,String name,int age){
+        this.email=email;
+        this.password=password;
+        this.name=name;
+        this.age=age;
+    }
+}
+class Child{
+    String email;
+    String password;
+    String name;
+    int age;
+    int cAmt=1000;
+
+    void Child(String email,String password,String name,int age){
+        this.email=email;
+        this.password=password;
+        this.name=name;
+        this.age=age;
+    }
+}
+class Main{
+    public static void main(String args[]){
+        System.out.println("");
+>>>>>>> 3551df3 (remote)
     }
 }
