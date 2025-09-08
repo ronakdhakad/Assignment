0b25cb851b8afdf741f7eb93a4711d68b603882c
ronakdhakad <dhakadr932@gmail.com>
2025-09-08 12:57:43 +0530

    pattern

--- assignment5/Q124.java
+++ assignment5/Q124.java
@@ -11,7 +11,7 @@ class Main{
                     System.out.print("_");
                 }
             }
-            System.out.println();
+            System.out.println("");
         }
     }
 }
