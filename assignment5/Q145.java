e96a670d05d61bde757c325900fdc1658cae8daa
ronakdhakad <dhakadr932@gmail.com>
2025-09-09 17:27:06 +0530

    a

--- assignment5/Q145.java
+++ assignment5/Q145.java
@@ -8,7 +8,15 @@ class Main{
     public static void main(String args[]){
         for(int i=1;i<=5;i++){
             for(int j=1;j<=5-i;j++){
-                
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i*2;k++){
+                if(k==1){
+                    System.out.print(k);
+                }else{
+                    System.out.print(k);
+                    System.out.print();
+                }
             }
         }
     }
