c015e5ec3afaa9396c6f8a7e27fd44a56693c139
Deepak <deepak.dhakad@silvergrey.in>
2025-10-01 12:05:12 +0530

    String

---
+++ practiceQ/Test.java
@@ -0,0 +1,8 @@
+public class Test{
+    public static void main(String[] args) {
+        String s5 = new String();
+        
+        System.out.println("s5 = '" + s5 + "'");  // prints: s5 = ''
+        System.out.println("Length = " + s5.length()); // prints: 0
+    }
+}
