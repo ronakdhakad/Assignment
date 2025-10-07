66b8c633f985c48ca2ed887ba3e1c2df2093234c
ronakdhakad <dhakadr932@gmail.com>
2025-10-07 17:28:50 +0530

    Q6 - Assignment19

--- assignment19/Q6.java
+++ assignment19/Q6.java
@@ -1,91 +1,99 @@
 import java.util.Scanner;
 class Bank{
+    int accNo;
+    String name;
+    int bal=0; 
+
     Scanner sc=new Scanner(System.in);
     int wamt=0,damt=0;
 
     class Account{
-        int accNo;
-        String name;
-        int bal=0;       
-            void addAcc(){
-                System.out.print("Account Number: ");
-                accNo=sc.nextInt();
-                sc.nextLine();
-                System.out.print("Account Holder Name: ");
-                name=sc.nextLine();
-                System.out.print("Balance: ");
-                bal=sc.nextInt();
-            }
-    }
+        void addAcc(){
+            System.out.print("Account Number: ");
+            accNo=sc.nextInt();
+            sc.nextLine();
+            System.out.print("Account Holder Name: ");
+            name=sc.nextLine();
+            System.out.print("Balance: ");
+            bal=sc.nextInt();
+        }
+
     void transaction(){
-        System.out.print("Do you want to- Withdraw press (1) or Deposite press (2) : ");
+        System.out.print("Do you want to- Withdraw press (1): ");
         int press =sc.nextInt();
-        if(press==2){
+        if(press==1){
             System.out.print("Enter deposite Amount : ");
             damt=sc.nextInt();
-            aobj.bal=aobj.bal-damt;
+            bal=bal-damt;
         }
-        if(press==1){
+        System.out.print("Do you want to- Deposite press (2) : ");
+        press =sc.nextInt();
+        if(press==2){
             System.out.print("Enter withdraw Amount : ");
             wamt=sc.nextInt();
-            aobj.bal=aobj.bal-wamt;
+            bal=bal-wamt;
         }
     }
-    Account aobj = new Account();
-    void display(){
-        System.out.print("Account Holder Number: "+aobj.accNo+"\n");
-        System.out.print("Holder Name: "+aobj.name+"\n");
-        System.out.print("Balance: "+aobj.bal+"\n");
-        System.out.println();
-    }
-    int i=1;
-    void transDisplay(){
+        void display(){
+            System.out.print("Account Holder Number: "+accNo+"\n");
+            System.out.print("Holder Name: "+name+"\n");
+            System.out.print("Balance: "+bal+"\n");
+            System.out.println();
+        }
         int i=1;
-        System.out.println("-- Account Transaction -- ");
-        System.out.println(i+". Account "+aobj.accNo+" created");
-        System.out.println((i+1)+". $"+damt+" deposited");
-        System.out.println((i+2)+". $"+damt+" withdrew");
-        System.out.println();
-    }
-    void summary(){
-        System.out.println(i+". Account  -- "+aobj.accNo);
-        i++;
-        System.out.println();
-    }
-    static class AuditTrail{
-        Bank bobj1= new Bank();
-        int i=1;
-        void logTrans(){
-            bobj1.transDisplay();
+        void transDisplay(){
+            System.out.println("-- Account Transaction -- ");
+            System.out.println(i+". Account "+accNo+" created");
+            System.out.println((i+1)+". $"+damt+" deposited");
+            System.out.println((i+2)+". $"+damt+" withdrew");
             System.out.println();
         }
         void summary(){
-            bobj1.summary();
+            System.out.println(i+". Account  -- "+accNo);
+            i++;
             System.out.println();
         }
     }
-    public static void main(String args[]){
-        Bank bobj[] = new Bank[2];
-        Bank bobj1= new Bank();
-        Account aobj[] = new Account[2];
-        // Account aobj1 = new Account();
-
-        for(int i=0;i<aobj.length;i++){
-            aobj[i]=bobj1.new Account();
-            aobj[i].addAcc();
-            bobj[i]=new Bank();
-            bobj[i].transaction();
+    static class AuditTrail{
+        Bank bobj= new Bank();
+        Account aobj = bobj.new Account();
+        int i=1;
+        void logTrans(){
+            aobj.transDisplay();
             System.out.println();
         }
-
-        for(Bank b:bobj){
-            b.display();
+        void summary(){
+            aobj.summary();
             System.out.println();
-            AuditTrail aobj2=new AuditTrail();
-            // for(int i=0;i<aobj.length;i++){
-                aobj2.logTrans();
-                System.out.println();
-            // }
         }
     }
 }
+class Main extends Bank{
+    public static void main(String args[]){
+        Bank bobj[]= new Bank[2];
+        Bank bobj1= new Bank();
+        Account aobj[] = new Account[2];
+    
+        for(int i=0;i<bobj.length;i++){
+            bobj[i]=new Bank();
+            aobj[i]=bobj1.new Account();
+            aobj[i].addAcc();
+            bobj[i]=new Bank();
+            // bobj[i].transaction();
+            System.out.println();
+        }
+    
+        for(Bank b:bobj){
+            // b.display();
+            System.out.println();
+            AuditTrail aobj2=new AuditTrail();
+                aobj2.logTrans();
+                // System.out.println();
+         }
+         for(Account a:aobj){
+            a.display();
+         }
+
+    }
+    }
+    
